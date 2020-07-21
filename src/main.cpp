34227307b7d3e8a10ad369a4ffaedff42bfbb271
André Wählisch <andre.waehlisch@physik.tu-berlin.de>
2020-07-21 23:52:08 +0200

    build window title from command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,20 @@
 #include <QDesktopWidget>
 
 int main(int argc, char *argv[]) {
-    Q_INIT_RESOURCE(images); // init resources (needed for static build)
-    QApplication app(argc, argv); // create the QApplication (there can only be a single one)
-    app.setQuitOnLastWindowClosed(false);  // if all windows (timers) are hidden because they are minimized to tray and then one timer is closed, then the app would close without this
+	Q_INIT_RESOURCE(images); // init resources (needed for static build)
+	QApplication app(argc, argv); // create the QApplication (there can only be a single one)
+	app.setQuitOnLastWindowClosed(false);  // if all windows (timers) are hidden because they are minimized to tray and then one timer is closed, then the app would close without this
 
-    MainWindow::theIcon = QIcon(":/hourglass.ico");
+	MainWindow::theIcon = QIcon(":/hourglass.ico");
 
-    MainWindow *w = new MainWindow(); // create the first default timer window (gets auto deleted)
-    w->show();
+	QStringList arguments = app.arguments();
 
-    return app.exec();
+	// create the first default timer window (gets auto deleted)
+	MainWindow *w = new MainWindow();
+	if ( arguments.size() > 1 ) {
+		w->setWindowTitle(arguments.mid(1).join(' ')); // build the window title from command line arguments (except the 0th, i.e, the program name)
+	}
+	w->show();
+
+	return app.exec();
 }
